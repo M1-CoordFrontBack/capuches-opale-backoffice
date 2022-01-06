06101e5c5b8bec94bf05f3f0a0f151e524bdadea
Maël Debon <mael.debon@owkin.com>
2022-01-06 14:51:54 +0100

    Update data format & add icons

--- src/pages/Quests.vue
+++ src/pages/Quests.vue
@@ -1,108 +1,151 @@
 <template>
-    <div class="row">
-      <div class="col-12">
-        <card :title="table1.title">
-          <div class="table-responsive">
-            <base-table :data="table1.data"
-                        :columns="table1.columns"
-                        thead-classes="text-primary">
-            </base-table>
-          </div>
-        </card>
-      </div>
-
+  <div class="row">
+    <div class="col-12">
+      <card :title="table1.title">
+        <div class="table-responsive">
+          <base-table
+            :data="table1.data"
+            :columns="table1.columns"
+            thead-classes="text-primary"
+          >
+          </base-table>
+        </div>
+      </card>
     </div>
+  </div>
 </template>
 <script>
 import { BaseTable } from "@/components";
 const tableColumns = [
-  {"name": "titre", "width": "55%"},
-  {"name": "récompense", "width": "30%"},
-  {"name": "état", "width": "15%"}
+  { name: "title", width: "55%", id: "title" },
+  { name: "reward", width: "30%", id: "reward_gold" },
+  { name: "status", width: "15%", id: "status" },
 ];
 const tableData = [
   {
     id: 1,
-    titre: "Acheter du lait",
-    récompense: "$23",
-    état: "Proposée",
-    durée: "3 jours",
-    nombre: "3",
-    aventuriers: ['Camille', 'Marius', 'Maël'],
-    description: "J'aimerais beaucoup acheter du lait pour pouvoir faire un mugcake",
+    title: "Acheter du lait",
+    reward_gold: 23,
+    reward_exp: 200,
+    status: "Proposée",
+    duration: "3 jours",
+    expiration: "01/04/22",
+    persons: 3,
+    aventurers: ["Camille", "Marius", "Maël"],
+    description:
+      "J'aimerais beaucoup aller acheter du lait pour pouvoir réaliser un mugcake au chocolat !",
     niv: {
       artisan: "2",
       melee: "0",
       archer: "0",
-      mage: "22"
-    }
+      mage: "22",
+    },
   },
   {
     id: 2,
-    titre: "Sauver un chaton",
-    récompense: "$41",
-    état: "En cours",
-    durée: "",
-    nombre: 0,
-    aventuriers: [],
-    description: ""
+    title: "Sauver un chaton",
+    reward_gold: 10,
+    reward_exp: 20,
+    status: "En cours",
+    duration: "",
+    expiration: "01/03/22",
+    persons: 0,
+    aventurers: [],
+    description: "",
+    niv: {
+      artisan: "2",
+      melee: "0",
+      archer: "0",
+      mage: "22",
+    },
   },
   {
     id: 3,
-    titre: "Relever le pouvoir d'achat des Français",
-    récompense: "$23.000.000",
-    état: "Echouée",
-    durée: "",
-    nombre: 0,
-    aventuriers: [],
-    description: ""
+    title: "Relever le pouvoir d'achat des Français",
+    reward_gold: 23000000,
+    reward_exp: 2,
+    status: "Echouée",
+    duration: "",
+    expiration: "01/03/22",
+    persons: 0,
+    aventurers: [],
+    description: "",
+    niv: {
+      artisan: "2",
+      melee: "0",
+      archer: "0",
+      mage: "22",
+    },
   },
   {
     id: 4,
-    titre: "Tuer un dragon",
-    récompense: "$2",
-    état: "Validée",
-    durée: "",
-    nombre: 0,
-    aventuriers: [],
-    description: ""
+    title: "Tuer un dragon",
+    reward_gold: 2,
+    reward_exp: 0,
+    status: "Validée",
+    duration: "",
+    expiration: "01/03/22",
+    persons: 0,
+    aventurers: [],
+    description: "",
+    niv: {
+      artisan: "2",
+      melee: "0",
+      archer: "0",
+      mage: "22",
+    },
   },
   {
     id: 5,
-    titre: "Acheter de l'hyxdroxichloroquine",
-    récompense: "$135",
-    état: "Réussie",
-    durée: "",
-    nombre: 0,
-    aventuriers: [],
-    description: ""
+    title: "Acheter de l'hyxdroxichloroquine",
+    reward_gold: 135,
+    reward_exp: 0,
+    status: "Réussie",
+    duration: "",
+    expiration: "01/03/22",
+    persons: 0,
+    aventurers: [],
+    description: "",
+    niv: {
+      artisan: "2",
+      melee: "0",
+      archer: "0",
+      mage: "22",
+    },
   },
   {
     id: 6,
-    titre: "Lorem ipsum",
-    récompense: "$120",
-    état: "Refusée",
-    durée: "",
-    nombre: 0,
-    aventuriers: [],
-    description: ""
-  }
+    title: "Lorem ipsum",
+    reward_gold: 120,
+    reward_exp: 12,
+    status: "Refusée",
+    duration: "",
+    expiration: "01/03/22",
+    persons: 0,
+    aventurers: [],
+    description: "",
+    niv: {
+      artisan: "2",
+      melee: "0",
+      archer: "0",
+      mage: "22",
+    },
+  },
 ];
 
 export default {
   components: {
-    BaseTable
+    BaseTable,
   },
   data() {
     return {
       table1: {
         title: "Liste des quêtes",
         columns: [...tableColumns],
-        data: [...tableData]
-      }
+        data: [...tableData],
+      },
     };
-  }
+  },
 };
 </script>
-<style>
-</style>
+<style></style>
