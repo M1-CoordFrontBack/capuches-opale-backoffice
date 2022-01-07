c1af5de465332078419aa7152c6516a149216cc7
camillenaulet03 <45195515+camillenaulet03@users.noreply.github.com>
2022-01-07 15:24:08 +0100

    DEV-62 ajout header site interne et public (#9)
    * DEV-62 ajout header site interne et public
    
    * DEV-62 ajout header site interne et public

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="header">
-    <router-link to="/"><img src="../../assets/svg/logo.svg" alt="logo"/></router-link>
+    <h4 class="title">Quête</h4>
     <div class="navmenu">
       <router-link to="/" class="menu">Mon compte</router-link>
       <router-link to="/" class="menu">Liste des quêtes</router-link>
       <router-link to="/create-quest" class="menu"><button>Ajouter une quête</button></router-link>
-      <router-link to="/"><img src="../../assets/img/logout.png" alt="logout" /></router-link>
+      <router-link to="/"><img src="../assets/img/logout.png" alt="logout" /></router-link>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-  @import "../../assets/sass/front-office/Header";
+  @import "../assets/sass/back-office/Header";
 </style>
