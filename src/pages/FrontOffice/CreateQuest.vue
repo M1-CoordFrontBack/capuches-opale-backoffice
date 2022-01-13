388d0ff0f932aaa4beb2d926742b4d1df43c9f86
kana03 <camille.naulet@hotmail.fr>
2022-01-13 13:54:20 +0100

    fix error message on create quest

--- src/pages/FrontOffice/CreateQuest.vue
+++ src/pages/FrontOffice/CreateQuest.vue
@@ -7,39 +7,39 @@
           <tr>
             <td>
               <label>Titre</label>&nbsp;<span class="error" v-if="titleError">Titre requis.</span><br />
-              <input type="text" id="title" v-model="this.title" />
+              <input type="text" id="title" v-model="title" />
             </td>
             <td rowspan="4">
               <label>Description</label>&nbsp;<span class="error" v-if="descriptionError">Description requis.</span><br />
-              <textarea id="description" v-model="this.description"></textarea>
+              <textarea id="description" v-model="description"></textarea>
             </td>
           </tr>
           <tr>
             <td>
               <label>Récompense</label>&nbsp;<span class="error" v-if="rewardError">Récompense requise.</span><br />
-              <input type="text" id="reward" v-model="this.reward" />
+              <input type="text" id="reward" v-model="reward" />
             </td>
           </tr>
           <tr>
             <td>
               <label>Durée estimée</label>&nbsp;<span class="error" v-if="estimateDateError">Date estimée requise..</span><br />
-              <input type="number" id="days" class="number" v-model="this.days" /> jours
-              <input type="number" id="hours" class="number" v-model="this.hours" /> heures
+              <input type="number" id="days" class="number" v-model="days" /> jours
+              <input type="number" id="hours" class="number" v-model="hours" /> heures
             </td>
           </tr>
           <tr>
             <td>
               <label>Date limite</label>&nbsp;<span class="error" v-if="limitDateError">Date limite requise.</span><br />
-              <input type="date" id="date" v-model="this.date" />
+              <input type="date" id="date" v-model="date" />
             </td>
           </tr>
           <tr>
             <td>
-              <input type="number" id="numberPeople" class="number" v-model="this.numberPeople" /> Nombre de personnes minimum &nbsp;<span class="error" v-if="numberPeopleError">Nombre de personnes requis.</span><br />
+              <input type="number" id="numberPeople" class="number" v-model="numberPeople" /> Nombre de personnes minimum &nbsp;<span class="error" v-if="numberPeopleError">Nombre de personnes requis.</span><br />
             </td>
             <td>
               <label>Commanditaire</label>&nbsp;<span class="error" v-if="clientError">Commanditaire requis.</span><br />
-              <input class="client" list="clients" placeholder="Commanditaire" v-model="this.client" />
+              <input class="client" list="clients" placeholder="Commanditaire" v-model="client" />
               <datalist id="clients">
                 <option v-for="client in clients" :key="client.id">{{client.login}}_{{client.firstname}}_{{client.firstname}}</option>
               </datalist>
@@ -60,10 +60,10 @@
           </tr>
           <tr>
             <td>
-              <input type="number" id="melee" class="number levelinput" v-model="this.melee"/>
-              <input type="number" id="archer" class="number levelinput" v-model="this.archer"/>
-              <input type="number" id="mage" class="number levelinput" v-model="this.mage"/>
-              <input type="number" id="artisan" class="number levelinput" v-model="this.artisan"/>
+              <input type="number" id="melee" class="number levelinput" v-model="melee"/>
+              <input type="number" id="archer" class="number levelinput" v-model="archer"/>
+              <input type="number" id="mage" class="number levelinput" v-model="mage"/>
+              <input type="number" id="artisan" class="number levelinput" v-model="artisan"/>
             </td>
             <td class="button">
               <button id="add" v-on:click.prevent="checkForm">Ajouter</button>
@@ -136,14 +136,14 @@
           if (!this.artisan) this.artisan = 0;
         }
 
-        if (!this.title) this.titleError = true;
-        if (!this.description) this.descriptionError = true;
-        if (!this.reward) this.rewardError = true;
-        if (!this.days && !this.hours) this.estimateDateError = true;
-        if (!this.date) this.limitDateError = true;
-        if (!this.numberPeople) this.numberPeopleError = true;
-        if (!this.client) this.clientError = true;
-        if (!this.melee && !this.archer && !this.mage && !this.artisan) this.levelError = true;
+        this.titleError = !this.title;
+        this.descriptionError = !this.description;
+        this.rewardError = !this.reward;
+        this.estimateDateError = !this.days && !this.hours;
+        this.limitDateError = !this.date;
+        this.numberPeopleError = !this.numberPeople;
+        this.clientError = !this.client;
+        this.levelError = !this.melee && !this.archer && !this.mage && !this.artisan;
       }
     }
   }
