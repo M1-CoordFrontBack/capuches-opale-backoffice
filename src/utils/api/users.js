78b04d7f156c6ac6e6b4b0702a2b8c1d52d0b2df
Maël Debon <mael.dbn@gmail.com>
2022-01-14 14:48:37 +0100

    Progress

--- src/utils/api/users.js
+++ src/utils/api/users.js
@@ -1,8 +1,11 @@
 import { get } from '../fetch';
 
 const rootUrl = process.env.BACKEND_API_URL_USERS
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxMSwibG9naW4iOiJtYWVsIiwibm9tIjoiZGVib24iLCJwcmVub20iOiJtYWVsIiwiYXJnZW50IjpudWxsLCJtb3RfZGVfcGFzc2UiOiIkMmIkMTAkSVY4SzdLaTBjOEt2VnRJaDFSd1pCZXdIQlhhSzYyZWdYZ3pLWUpjSllLV3VPbnRQemFyYWkiLCJyb2xlX2lkIjoyfSwiaWF0IjoxNjQyMTY3MTQzLCJleHAiOjE2NDIxNzQzNDN9.v35VtmXIgSPujk6GxUmz82UDDgMBgUGpRuPWXMXjK_8";
+// expiration : 2h
 
-const getUsername = () => {
-    const data = get(rootUrl + '/username');
+export const getUsername = () => {
+    const data = get('https://hidden-headland-61940.herokuapp.com/' + 'api/user/11', token);
+    console.log(data);
     return data;
 }
