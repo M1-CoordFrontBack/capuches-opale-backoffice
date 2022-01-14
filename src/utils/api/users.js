71c6c6bf94259b713a3d43b6947ac9d643dd7335
kana03 <camille.naulet@hotmail.fr>
2022-01-14 16:14:19 +0100

    localstorage

--- src/utils/api/users.js
+++ src/utils/api/users.js
@@ -1,4 +1,4 @@
-import { get } from '../fetch';
+import { get } from '../functions';
 
 const rootUrl = process.env.BACKEND_API_URL_USERS
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxMSwibG9naW4iOiJtYWVsIiwibm9tIjoiZGVib24iLCJwcmVub20iOiJtYWVsIiwiYXJnZW50IjpudWxsLCJtb3RfZGVfcGFzc2UiOiIkMmIkMTAkSVY4SzdLaTBjOEt2VnRJaDFSd1pCZXdIQlhhSzYyZWdYZ3pLWUpjSllLV3VPbnRQemFyYWkiLCJyb2xlX2lkIjoyfSwiaWF0IjoxNjQyMTY3MTQzLCJleHAiOjE2NDIxNzQzNDN9.v35VtmXIgSPujk6GxUmz82UDDgMBgUGpRuPWXMXjK_8";
