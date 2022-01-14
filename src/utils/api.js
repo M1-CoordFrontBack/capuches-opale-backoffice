ac27f54708b0078eefb9f3a03e7386541c2b318a
Maël Debon <mael.dbn@gmail.com>
2022-01-14 11:03:29 +0100

    Add default functions

---
+++ src/utils/api.js
@@ -0,0 +1,6 @@
+import { get } from './fetch';
+
+async const getUsername = () => {
+    const data = get('https://localhost:8000/api/users/name');
+    console.log(data);
+}g
---
+++ src/utils/fetch.js
@@ -0,0 +1,17 @@
+export const get = (url) => {
+    fetch(url)
+      .then(function (response) {
+        if (response.status !== 200) {
+          console.log("Request failed. Status code: " + response.status);
+          return;
+        }
+  
+        response.json().then(function (data) {
+          console.log(data);
+        });
+      })
+      .catch(function (err) {
+        console.log("Fetch Error :-S", err);
+      });
+  };
+  
