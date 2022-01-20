af1def93873851c485484398fc43a531b5d6991d
Maël Debon <mael.dbn@gmail.com>
2022-01-20 15:29:09 +0100

    Add missing file

---
+++ src/utils/services/quests.js
@@ -0,0 +1,7 @@
+import { get } from '../functions';
+import localStorageService from "@/services/localStorageService";
+
+export const getQuestsByUser = () => {
+    return get(`https://quiet-fortress-49850.herokuapp.com/api/requete/clients/2`, localStorageService.getAccessToken());
+
+}
