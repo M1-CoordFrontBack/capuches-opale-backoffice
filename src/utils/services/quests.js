366be4804c92fb2f35603ce91590aa2ea831a1fd
Maël Debon <mael.dbn@gmail.com>
2022-01-21 16:47:51 +0100

    Fix user id in request

--- src/utils/services/quests.js
+++ src/utils/services/quests.js
@@ -2,6 +2,6 @@ import { get } from '../functions';
 import localStorageService from "@/services/localStorageService";
 
 export const getQuestsByUser = () => {
-    return get(`https://quiet-fortress-49850.herokuapp.com/api/requete/clients/2`, localStorageService.getAccessToken());
+    return get(`https://quiet-fortress-49850.herokuapp.com/api/requete/clients/${localStorageService.getUserId()}`, localStorageService.getAccessToken());
 
 }
